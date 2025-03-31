009caa8e6e6a55957cb617047b7a6e3048e421c2
Crusader <denisminakov58@gmail.com>
2025-04-01 01:02:53 +0300

    new graphics

--- tests/examples.py
+++ tests/examples.py
@@ -11,33 +11,56 @@ sys.path.append(str(Path(__file__).parent.parent))
 import gradient_descent as gd  # Теперь импорт работает
 import learning_rate_scheduling as lrs
 import stop_criteria as sc
+import plotly.graph_objects as go
 
 YOUR_DIR_NAME = os.path.abspath('../ex_graphics/')
 
 
 class Example:
-    def __painting_contour_lines(self, x, y, z, file_name):
-        pass
 
-    def __painting_3d(self, x, y, z, file_name):
-        if not os.path.exists(file_name):
-            with open(file_name, "wb") as f:
-                pass
+    # def __painting_3d_with_mathplotlib(self, x, y, z, file_name):
+    #     if not os.path.exists(file_name):
+    #         with open(file_name, "wb") as f:
+    #             pass
 
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(projection='3d')
 
-        anw = [x[len(x) - 1], y[len(y) - 1], z[len(z) - 1]]
-        ax.scatter(anw[0], anw[1], anw[2], c='b', marker='o')
-        ax.scatter(x, y, z, c='r', marker='o')
+    #     anw = [x[len(x) - 1], y[len(y) - 1], z[len(z) - 1]]
+    #     ax.scatter(anw[0], anw[1], anw[2], c='b', marker='o')
+    #     ax.scatter(x, y, z, c='r', marker='o')
 
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.set_title('Траектория градиентного спуска')
-        # plt.show()
-        # plt.show()
-        plt.savefig(file_name)
+    #     ax.set_xlabel('X')
+    #     ax.set_ylabel('Y')
+    #     ax.set_zlabel('Z')
+    #     ax.set_title('Траектория градиентного спуска')
+    #     # plt.show()
+    #     # plt.show()
+    #     plt.savefig(file_name)
+        
+    def __painting_3d_with_plotly(self, x, y, z, file_name, function):
+        row = np.linspace(max(min(x), -50), min(max(x), 50), 1)  # 50 точек вместо случайных
+        col = np.linspace(max(min(y), -50), min(max(y), 50), 1)
+        X, Y = np.meshgrid(row, col)
+
+        Z = np.array([[function((x_i, y_j)) for x_i in row] for y_j in col])
+        
+        # поверхность
+        surface = go.Surface(x = X, y = Y, z = Z,
+                             opacity=0.7,
+                             contours_z={'show': True})        
+        # print(len(x), len(y), len(z))
+        # >> 135 135 135    
+        # добавить линию
+        scatter = go.Scatter3d(x=x, y=y, z=z,
+                mode='lines+markers',
+                line=dict(color='red', width=5),
+                marker=dict(size=3)
+            )
+        
+        # управляет изображением
+        fig = go.Figure(data=[scatter, surface])
+        fig.show()
 
     def run_example(self,
                     dimension=2,
@@ -50,11 +73,11 @@ class Example:
         descent = gd.gradient_descent(dimension, function, gradient, test_criterion, learning_rate_scheduling)
         descent.make_min_value(beginning_point)
         lst = [point for (_, point) in descent.get_log()]
-        x = [point[0] for point in lst]
+        x = [point[0] for point in lst] 
         y = [point[1] for point in lst]
         z = [function(point) for point in lst]
         anw = [x[len(x) - 1], y[len(y) - 1], z[len(z) - 1]]
-        self.__painting_3d(x, y, z, file_name)
+        self.__painting_3d_with_plotly(x, y, z, file_name, function)
         return anw
 
 
@@ -82,6 +105,5 @@ def create_dir():
 
 if __name__ == "__main__":
     create_dir()
-    generator = Generate_test(10)
+    generator = Generate_test(1)
     generator.generate()
-    # print(YOUR_FILE_NAME)
