010d6a45ab7e1c900cd207f3c0c4dd12eeb133a4
Matveiiy <51031652+Matveiiy@users.noreply.github.com>
2025-04-10 15:18:53 +0300

    Update gradient.py

--- gradient.py
+++ gradient.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 class find_gradient:
 
     def __init__(self, function):
@@ -15,3 +15,20 @@ class find_gradient:
             point[i] -= delta
 
         return anw / delta
+
+    def __find_norm(point: np.array) -> np.array:
+        square = point * point
+        return math.sqrt(square.sum())
+    
+    def get_modifier_value(self, point: np.array, delta=1e-6, upper_bound=10) -> np.array:
+        default_value = self.function(point)
+        anw = np.zeros_like(point)
+
+        for i in range(len(point)):
+            point[i] += delta
+            anw[i] = self.function(point) - default_value
+            point[i] -= delta
+        grad = anw / delta
+        while (self.__find_norm(grad) > upper_bound):
+            grad = np.random.random(len(point))
+        return grad
