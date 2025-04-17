ec3ddb241c4549f5e5fb44375958e7f4815ea46e
Matveiiy <51031652+Matveiiy@users.noreply.github.com>
2025-04-17 19:32:37 +0300

    update

--- tests/examples.py
+++ tests/examples.py
@@ -5,11 +5,14 @@ import numpy as np
 
 import sys
 from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
+
+import line_search as ls
 from learning_rate_scheduling import LRScheduler, Constant, StepDecay
+
 # from line_search import GoldenSectionSearch, ArmijoRule, GoldsteinRule, SciPyLineSearch
 
-sys.path.append(str(Path(__file__).parent.parent))
 import math
 import gradient_descent as gd
 import learning_rate_scheduling as lrs
@@ -21,10 +24,10 @@ YOUR_DIR_NAME = os.path.abspath('../ex_graphics/')
 
 
 class Example:
-    
+
     def __painting_3d_with_plotly(self, x, y, z, file_name, function):
-        x_min, x_max, y_min, y_max = min(x), max(x), min(y), max(y) 
-        
+        x_min, x_max, y_min, y_max = min(x), max(x), min(y), max(y)
+
         x_padding, y_padding = (x_max - x_min) * 0.5, (y_max - y_min) * 0.5
 
         row = np.linspace(x_min - x_padding, x_max + x_padding, 50)
@@ -32,24 +35,24 @@ class Example:
         X, Y = np.meshgrid(row, col)
 
         Z = np.array([[function((x_i, y_j)) for x_i in row] for y_j in col])
-        
+
         # поверхность
         surface = go.Surface(x = X, y = Y, z = Z,
                              opacity=0.7,
-                             contours_z={'show': True})        
-  
+                             contours_z={'show': True})
+
         # добавить линию
         scatter = go.Scatter3d(x=x, y=y, z=z,
-                mode='lines+markers',
-                line=dict(color='red', width=5),
-                marker=dict(size=3)
-            )
-        
+                               mode='lines+markers',
+                               line=dict(color='red', width=5),
+                               marker=dict(size=3)
+                               )
+
         # добавляем последнюю точку
         last_point = go.Scatter3d(x=[x[-1]], y=[y[-1]], z=[z[-1]],
-            mode='markers',
-            marker=dict(color='green', size=6)
-        )   
+                                  mode='markers',
+                                  marker=dict(color='green', size=6)
+                                  )
         # управляет изображением
         fig = go.Figure(data=[scatter, surface, last_point])
         fig.show()
@@ -61,11 +64,11 @@ class Example:
                     test_criterion: sc.StoppingCriterion =sc.Convergence(),
                     learning_rate_scheduling: LRScheduler = Constant(),
                     file_name='/home/crusader/ml_yandex/Gradient-descent/graphics/first_ex.png',
-                    beginning_point=None) -> list:
+                    beginning_point=None):
         descent = gd.gradient_descent(dimension, function, gradient, test_criterion, learning_rate_scheduling)
         descent.make_min_value(beginning_point)
         lst = [point for (_, point) in descent.get_enum_point()]
-        x = [point[0] for point in lst] 
+        x = [point[0] for point in lst]
         y = [point[1] for point in lst]
         z = [function(point) for point in lst]
         anw = [x[len(x) - 1], y[len(y) - 1], z[len(z) - 1]]
@@ -84,7 +87,7 @@ class Generate_test:
         for num in range(self.count):
             [alpha, bravo, charlie] = np.random.randint(1, 10, size=3)
             fun_gen = (lambda point: alpha * point[0] ** 2 + bravo * point[1] ** 2 + charlie * point[1] *
-                                                  point[0])
+                                     point[0])
             self.ex.run_example(dimension=2,
                                 function=fun_gen,
                                 gradient=None,
@@ -100,18 +103,18 @@ def create_dir():
 def ex_sample(func, test_criterion = sc.Convergence(), gradient = None, beginning_point = np.array([10., 10.])):
     for i in (lrs.Constant(), lrs.TimeBasedDecay(), lrs.ExponentialDecay()):
         anw = ex.run_example(dimension=2,
-                       function=func,
-                       gradient=gradient,
-                       learning_rate_scheduling=i,
-                       test_criterion=test_criterion,
-                       beginning_point=beginning_point)[0]
+                             function=func,
+                             gradient=gradient,
+                             learning_rate_scheduling=i,
+                             test_criterion=test_criterion,
+                             beginning_point=beginning_point)[0]
         print(f'anw = {anw}')
 
 def first_ex():
     func = lambda x: x[0]**2 + x[1]**2
     ex_sample(func, sc.Convergence())
-    
-    
+
+
 def second_ex():
     """
     bad function for this graphic
@@ -125,53 +128,61 @@ def third_ex():
     """
     func = lambda x:  x[0]**2 + x[1] - 1
     ex_sample(func, sc.MaxIterations(200))
-    
+
 def fourth_ex():
     """
     strange_function
     """
     func = lambda x: 100 * math.sqrt(abs(x[1] - 0.01 * x[0]**2)) + 0.01 * abs(x[0] + 10)
-    ex_sample(func, sc.MaxIterations(200))
-
-def fivth_ex():
-    """
-    rozenbrok function
-    """
-    func = lambda x: (1 - x[0]) ** 2 + 100 * (x[1] - x[0]**2)**2 
-    ex_sample(func, sc.MaxIterations(200), gradient= lambda x: 
-        np.array([4 * (x[0]**3) - 400 * x[0] * x[1] - 2 + 2 * x[0], 200 * x[1] - 200 * (x[0]**2)]),
-        beginning_point=np.array([1., 1.]))
-
-def sixth_ex():
-    """
-    one more good function
-    """
-    func = lambda x: x[0] ** 2 + x[1] ** 2 + (x[0] ** 2) * x[1] + x[0] ** 4
-    ex_sample(func, beginning_point=np.array([2., 2.]))
-
-def seventh_ex():
-    """
-    Happy cat function
-    """
-    func = lambda x: (((x[0]**2 + x[1]**2)**(1/2) - 2)**2)**(1/8) + 1/2*(1/2*(x[0]**2+x[1]**2)**(1/2) + x[0] + x[1]) + 1/2
-    ex_sample(func, sc.MaxIterations(200))
+    ex_sample(func, sc.Combine(sc.MaxIterations(10000), sc.Convergence()))
 
 def eight_ex():
     """
     noisy function
-    """ 
+    """
     func = lambda x: x[0]**2 + x[1]**2 + random.random()
-    ex_sample(func, sc.MaxIterations(300))
+    ex_sample(func, sc.MaxIterations(200))
+
+
+
+def ex_sample1(func, str_decay, gradient=None, beginning_point=np.array([10., 10.])):
+    mp2 = {
+        "time" : lrs.TimeBasedDecay(),
+        "exp" : lrs.ExponentialDecay(),
+        "const" : lrs.Constant(),
+        "poly"  : lrs.PolynomialDecay(),
+
+        "armijo" : ls.ArmijoRule(function=func),
+        "goldstein" : ls.GoldsteinRule(function=func),
+        "goldensection" :  ls.GoldenSectionSearch(function=func)
+    }
+    anw = ex.run_example(dimension=2,
+                         function=func,
+                         gradient=gradient,
+                         learning_rate_scheduling=mp2[str_decay],
+                         test_criterion=sc.MaxIterations(300),
+                         beginning_point=beginning_point)[0]
+    print(str_decay, anw)
+
+def run():
+    func = lambda x: x[0]**2 + x[1]**2
+    ex_sample1(func, "time")
+    ex_sample1(func, "exp")
+    ex_sample1(func, "const")
+    ex_sample1(func, "poly")
+def nine_ex():
+    """
+    one more
+    """
+    func = lambda x: 0.1 * x[0]**2 + 2 * x[1]**2
+    ex_sample(func, sc.Convergence())
 
 
 if __name__ == "__main__":
     ex = Example()
+    # nine_ex()
+    run()
+    # ex_sample1("armijo", "iter10000")
     # eight_ex()
-    # first_ex()
-    # second_ex()
-    # third_ex()
-    # fourth_ex()
-    # fivth_ex()
-    # sixth_ex()
-    # seventh_ex()
+
 
